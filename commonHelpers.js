073e4e2339e8135d07b9f8dd9b07bdbb0f1e8c1c
POWERSL2DE <141239495+POWERSL2DE@users.noreply.github.com>
2024-04-07 13:36:08 +0000

    Deploying to gh-pages from @ POWERSL2DE/goit-js-hw-11@1dc2b3615ad5e443b11b9c8861bad2ce45df7eb5 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,11 @@
+import{S as u,i as c}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const d=document.querySelector(".form"),f=document.querySelector(".gallery"),l=document.querySelector(".loader");let n;const m=new u(".gallery a",{captionsData:"alt",captionDelay:250,nav:!0,close:!0,enableKeyboard:!0,docClose:!0});function g(s){const t=new URLSearchParams({key:"41672793-a8580f18ed6f224a15f8d2674",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${t}`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()})}d.addEventListener("submit",s=>{if(s.preventDefault(),f.innerHTML="",l.classList.remove("visible"),n=s.target.elements.search.value.trim(),!n){c.error({title:"Error",message:"Sorry, imput is empty!",position:"topRight"}),l.classList.add("visible");return}g(n).then(({hits:t})=>{if(t.length===0){c.error({title:"Error",message:"❌ Sorry, there are no images matching your search query. Please try again!",position:"topRight",messageColor:"#ffffff",titleColor:"#ffffff",iconColor:"#ffffff",backgroundColor:"#EF4040"});return}const o=t.reduce((a,e)=>a+`<li class="gallery-item"> 
+        <a class="gallery-link" href="${e.largeImageURL}">
+        <img class="gallery-image" src="${e.webformatURL}" alt="${e.tags}"/></a>
+        <div class="gallery-info">
+        <p>likes: ${e.likes}</p> 
+        <p>views: ${e.views}</p>
+        <p>comments: ${e.comments}</p>
+        <p>downloads: ${e.downloads}</p>
+        </div>
+        </li>`,"");f.innerHTML=o,m.refresh()}).catch(t=>console.log(t)).finally(()=>{l.classList.add("visible")}),s.currentTarget.reset()});
+//# sourceMappingURL=commonHelpers.js.map
